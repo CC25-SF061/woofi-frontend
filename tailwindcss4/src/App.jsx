289bdd6eef9abe880ac773384e22e85e785374ad
sulih222 <sulihmragil@gmail.com>
2025-03-08 15:40:15 +0800

    initial react and tailwind

---
+++ tailwindcss4/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-red-500 text-4xl p-10 font-quicksand">hello world Q</p>
+      <p className="text-red-500 text-4xl p-10 font-julius-sans-one">hello world</p>
+      <p className="text-red-500 text-4xl p-10 font-inknut-antiqua">hello world</p>
+    </div>
+  );
+};
+
+export default App;
