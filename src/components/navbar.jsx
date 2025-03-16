a85736b3ba730981a51b5fbe43c9f2521b2fb9e9
Agung Mantra <agung.mantra07@gmail.com>
2025-03-16 17:30:52 +0800

    Update not found page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -109,7 +109,7 @@ const Navbar = () => {
           {/* Sign In Button */}
           <Link
             to="/sign-in"
-            className="px-4 py-2 border border-white rounded-md hover:bg-white hover:text-black transition"
+            className="px-4 py-2 border border-[#FFA666] rounded-md hover:bg-[#FFA666] hover:text-black transition"
           >
             Sign In
           </Link>
--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import logo from "../assets/navbar/finalLogo.png";
+import notFoundImage from "../assets/images/notFound.png"; 
 
-const notFound = () => {
+const NotFound = () => {
   return (
-    <div className=''>
-      <h1 className='text-white'>404 NOT FOUND</h1>
-    </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white relative">
+      {/* Logo */}
+      <div className="absolute top-5 left-5">
+        <img src={logo} alt="Logo" className="w-48"/>
+      </div>
 
-export default notFound
+      {/* Ilustrasi 404 dengan animasi bounce */}
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="w-full max-w-xl"
+      >
+        <motion.img 
+          src={notFoundImage} 
+          alt="404 Not Found" 
+          className="w-full h-auto"
+          animate={{
+            y: [0, -20, 0],
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 2,
+            ease: "easeInOut",
+          }}
+        />
+      </motion.div>
+
+      {/* Tombol Go Home */}
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="mt-5"
+      >
+        <Link
+          to="/"
+          className="px-6 py-3 bg-transparent text-white border border-orange-400 rounded-lg transition duration-300 hover:bg-orange-400 hover:text-gray-900"
+        >
+          Go Home
+        </Link>
+      </motion.div>
+    </div>
+  );
+};
+
+export default NotFound;
