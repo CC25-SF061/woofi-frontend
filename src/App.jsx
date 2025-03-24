3d05a508d5c164b7c81c078de91c0716ff825d3c
kesava <kesavadananjaya@gmail.com>
2025-03-25 01:36:42 +0800

    add integration api

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,66 @@
-import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import Pages
-import Home from "./pages/homePage";
-import CultureHistory from "./pages/cultureHistory";
-import GalleryPage from "./pages/galleryPage";
-import DestinationPage from "./pages/destinationPage";
-import NotFound from "./pages/notFound";
-import ContactUs from "./pages/contactUs";
-import JoinUs from "./pages/joinUs";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Home from './pages/homePage';
+import CultureHistory from './pages/cultureHistory';
+import GalleryPage from './pages/galleryPage';
+import DestinationPage from './pages/destinationPage';
+import NotFound from './pages/notFound';
+import ContactUs from './pages/contactUs';
+import JoinUs from './pages/joinUs';
 
-import Signin from "./pages/loginPages/signin";
-import Register from "./pages/loginPages/register";
-import ForgetPassword from "./pages/loginPages/forgetPassword";
+import Signin from './pages/loginPages/signin';
+import Register from './pages/loginPages/register';
+import ForgetPassword from './pages/loginPages/forgetPassword';
 
-import Profile from "./pages/profilePages/profile";
-import Wishlist from "./pages/profilePages/wishlist";
-import AddData from "./pages/profilePages/addData";
+import Profile from './pages/profilePages/profile';
+import Wishlist from './pages/profilePages/wishlist';
+import AddData from './pages/profilePages/addData';
 
-import ScrollToTop from "./scrollToTop";
-import PageWrapper from "./components/loading/pageWrapper";
+import ScrollToTop from './scrollToTop';
+import PageWrapper from './components/loading/pageWrapper';
+import { GoogleLogin } from '@react-oauth/google';
 
 const App = () => {
-  return (
-    <Router>
-      <ScrollToTop/>
-      <PageWrapper/>
-      <Routes>
-        <Route path='/' element ={<Home/>}></Route>
-        <Route path='/culture' element ={<CultureHistory/>}></Route>
-        <Route path='/destination' element ={<DestinationPage/>}></Route>
-        <Route path='/gallery' element ={<GalleryPage/>}></Route>
-        <Route path='/contact-us' element ={<ContactUs/>}></Route>
-        <Route path='/join-us' element ={<JoinUs/>}></Route>
-        <Route path='/sign-in' element ={<Signin/>}></Route>
-        <Route path='/register' element ={<Register/>}></Route>
-        <Route path='/forget-password' element ={<ForgetPassword/>}></Route>
-        <Route path='/profile/wishlist' element ={<Wishlist/>}></Route>
-        <Route path='/profile' element ={<Profile/>}></Route>
-        <Route path='/profile/add-data' element ={<AddData/>}></Route>
-        <Route path="*" element={<NotFound/>}></Route>
-      </Routes>
-    </Router>
-  );
+    return (
+        <GoogleOAuthProvider
+            clientId={import.meta.env.VITE_GOOGLE_WEB_CLIENT_ID}
+        >
+            <Router>
+                <ScrollToTop />
+                <PageWrapper />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/culture" element={<CultureHistory />}></Route>
+                    <Route
+                        path="/destination"
+                        element={<DestinationPage />}
+                    ></Route>
+                    <Route path="/gallery" element={<GalleryPage />}></Route>
+                    <Route path="/contact-us" element={<ContactUs />}></Route>
+                    <Route path="/join-us" element={<JoinUs />}></Route>
+                    <Route path="/sign-in" element={<Signin />}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                    <Route
+                        path="/forget-password"
+                        element={<ForgetPassword />}
+                    ></Route>
+                    <Route
+                        path="/profile/wishlist"
+                        element={<Wishlist />}
+                    ></Route>
+                    <Route path="/profile" element={<Profile />}></Route>
+                    <Route
+                        path="/profile/add-data"
+                        element={<AddData />}
+                    ></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </Router>
+        </GoogleOAuthProvider>
+    );
 };
 
 export default App;
--- src/pages/loginPages/signin.jsx
+++ src/pages/loginPages/signin.jsx
@@ -1,32 +1,109 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image1 from '../../assets/logIn/image1.webp';
 import BannerLogin from '../../components/bannerLogin';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import axios, { AxiosError } from 'axios';
+import ErrorConstant from '../../util/ErrorConstant';
+import { ErrorToast } from '../../components/toast';
+import {
+    GoogleOAuthProvider,
+    useGoogleLogin,
+    useGoogleOneTapLogin,
+} from '@react-oauth/google';
 
 const SignIn = () => {
-    useEffect(() => {
-        async function fetchApi() {
-            try {
-                const result = await axios.post('api/auth/login', {
-                    email: 'test@example.com',
-                });
-            } catch (e) {
-                if (e instanceof AxiosError) {
-                    //response jika error bentuknya seperti ini
-                    console.log(e.response.data);
-                }
+    const [invalidLoginState, setInvalidLoginState] = useState(null);
+    const [errState, setErrState] = useState({
+        password: null,
+        email: null,
+    });
+    const invalidLogin = () => {
+        setInvalidLoginState('Invalid Email or password');
+        setTimeout(() => {
+            setInvalidLoginState(null);
+        }, 9000);
+    };
+    const invalidFieldErr = (arr) => {
+        const newObjErr = {
+            password: null,
+            email: null,
+        };
+        for (const element of arr) {
+            if (newObjErr.hasOwnProperty(element.path[0])) {
+                newObjErr[element.path[0]] = element.message;
             }
         }
-        fetchApi();
-    }, []);
-    const submit = (e) => {
-        e.preventDefault();
-        console.log(e);
+        setErrState((state) => ({ ...state, ...newObjErr }));
     };
+    const submit = async (e) => {
+        e.preventDefault();
+        setInvalidLoginState(null);
+        setErrState((state) => ({
+            ...state,
+            password: null,
+            email: null,
+        }));
+        const request = Object.fromEntries(new FormData(e.target));
+        try {
+            const response = await axios.post('/api/auth/login', request);
+
+            //lakukan sesuatu ketika sukses
+        } catch (e) {
+            if (!(e instanceof AxiosError)) {
+                return;
+            }
+
+            const err = e.response.data.payload;
+
+            if (err.errCode === ErrorConstant.ERR_INVALID_FIELD) {
+                invalidFieldErr(err.fields);
+            }
+
+            if (err.errCode === ErrorConstant.ERR_INVALID_LOGIN) {
+                invalidLogin();
+            }
+        }
+    };
+    const createUserGoogle = async (accessToken) => {
+        try {
+            const response = (
+                await axios.post('/api/auth/register/google', {
+                    token: accessToken,
+                })
+            ).data;
+            localStorage.setItem('accessToken', response.data.token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    const googleLogin = useGoogleLogin({
+        onSuccess: async (credentialResponse) => {
+            try {
+                const response = (
+                    await axios.post('/api/auth/login/google', {
+                        token: credentialResponse.access_token,
+                    })
+                ).data;
+                localStorage.setItem('accessToken', response.data.token);
+            } catch (e) {
+                if (!(e instanceof AxiosError)) {
+                    return;
+                }
+                const response = e.response.data.payload;
+                if (response.errCode === ErrorConstant.ERR_USER_NOT_FOUND) {
+                    await createUserGoogle(credentialResponse.access_token);
+                }
+            }
+        },
+        onError: () => {
+            console.log('Login Failed');
+        },
+        scope: 'profile email',
+    });
     return (
         <section className="bg-[#221122] min-h-screen w-full flex items-center justify-center p-5 md:p-10">
+            {invalidLoginState && <ErrorToast message={invalidLoginState} />}
             <div className="flex flex-col md:flex-row w-full max-w-5xl rounded-lg overflow-hidden shadow-lg bg-[#252527] min-h-[650px]">
                 {/* Form Section */}
                 <form
@@ -40,13 +117,18 @@ const SignIn = () => {
                     <input
                         type="email"
                         placeholder="Email"
+                        name="email"
                         className="w-full p-3 font-quicksand mb-4 rounded text-white border border-white bg-transparent focus:outline-none"
                     />
+                    <div className="error">{errState.email}</div>
                     <input
                         type="password"
                         placeholder="Password"
+                        name="password"
                         className="w-full p-3 font-quicksand mb-4 rounded text-white border border-white bg-transparent focus:outline-none"
                     />
+                    <div className="error">{errState.password}</div>
+
                     <button className="w-full p-3 bg-[#FFA666] text-white font-quicksand rounded hover:bg-orange-500 transition">
                         Login
                     </button>
@@ -57,7 +139,11 @@ const SignIn = () => {
                         <hr className="flex-grow border-gray-400" />
                     </div>
 
-                    <button className="w-full flex items-center justify-center p-3 bg-white text-black font-quicksand rounded shadow hover:bg-gray-200 transition mb-4">
+                    <button
+                        type="button"
+                        onClick={googleLogin}
+                        className="w-full flex items-center justify-center p-3 bg-white text-black font-quicksand rounded shadow hover:bg-gray-200 transition mb-4"
+                    >
                         <FcGoogle className="text-2xl mr-2" /> Sign in with
                         Google
                     </button>
