9d57766b4036b444ebbec91b07e69942e89a2f32
sulih222 <sulihmragil@gmail.com>
2025-03-27 12:29:48 +0800

    spacing card detail

--- src/components/culture/cultureDetail.jsx
+++ src/components/culture/cultureDetail.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const modalVariants = {
@@ -14,21 +14,22 @@ const backdropVariants = {
 };
 
 const CultureDetail = ({ item, onClose }) => {
-    useEffect(() => {
-        if (item) {
-          document.body.style.overflow = "hidden"; // Matikan scroll saat modal terbuka
-        }
-        return () => {
-          document.body.style.overflow = "auto"; // Pulihkan scroll saat modal ditutup
-        };
-      }, [item]);
-    if (!item) return null;
+  useEffect(() => {
+    if (item) {
+      document.body.style.overflow = "hidden"; // Matikan scroll saat modal terbuka
+    }
+    return () => {
+      document.body.style.overflow = "auto"; // Pulihkan scroll saat modal ditutup
+    };
+  }, [item]);
+
+  if (!item) return null;
 
   return (
     <AnimatePresence>
       {/* Backdrop */}
       <motion.div
-        className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+        className="fixed inset-0 bg-black/30 backdrop-blur-md flex items-center justify-center z-50"
         variants={backdropVariants}
         initial="hidden"
         animate="visible"
@@ -37,21 +38,40 @@ const CultureDetail = ({ item, onClose }) => {
       >
         {/* Modal */}
         <motion.div
-          className="bg-[#252527] rounded-lg shadow-lg p-6 max-w-2xl max-h-[500px] h-full w-full relative"
+          className="bg-[#252527] rounded-lg shadow-lg p-12 max-w-2xl max-h-[500px] w-full relative"
           variants={modalVariants}
           initial="hidden"
           animate="visible"
           exit="exit"
           onClick={(e) => e.stopPropagation()} // Mencegah modal tertutup saat diklik
         >
-          
-          <button onClick={onClose} className="absolute top-1 right-1 hover:text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32"><path fill="currentColor" d="M16 2C8.2 2 2 8.2 2 16s6.2 14 14 14s14-6.2 14-14S23.8 2 16 2m0 26C9.4 28 4 22.6 4 16S9.4 4 16 4s12 5.4 12 12s-5.4 12-12 12"></path><path fill="currentColor" d="M21.4 23L16 17.6L10.6 23L9 21.4l5.4-5.4L9 10.6L10.6 9l5.4 5.4L21.4 9l1.6 1.6l-5.4 5.4l5.4 5.4z"></path></svg>
+          {/* Tombol Close */}
+          <button
+            onClick={onClose}
+            className="absolute top-4 right-4 text-gray-400 hover:text-gray-50 cursor-pointer transition duration-200"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"
+              />
+            </svg>
           </button>
 
           {/* Isi Modal */}
-          <img src={item.image} alt={item.name} className="w-full h-72 object-cover bg-center rounded-md" />
-          <h2 className="text-lg font-inknut-antiqua text-white font-bold mt-4">{item.name} - <span className="text-[#FFA666]">{item.from}</span></h2>
+          <img
+            src={item.image}
+            alt={item.name}
+            className="w-full h-72 object-cover bg-center rounded-md"
+          />
+          <h2 className="text-lg font-inknut-antiqua text-white my-4">
+            {item.name} - <span className="text-[#FFA666]">{item.from}</span>
+          </h2>
           <p className="text-white">{item.desc}</p>
         </motion.div>
       </motion.div>
