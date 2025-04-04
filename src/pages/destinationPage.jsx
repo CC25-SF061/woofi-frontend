c2f7c214c283d32e3dcb3c934860eac828a9c8c8
KelloVerra <71198584+KelloVerra@users.noreply.github.com>
2025-04-04 20:16:33 +0800

    made login modal as react component

--- src/pages/destinationPage.jsx
+++ src/pages/destinationPage.jsx
@@ -17,13 +17,12 @@ import { showLoading, hideLoading } from '../stores/loadingReducer';
 import axios, { AxiosError } from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import DestinationFilter from '../util/DestinationFilter';
+import LoginModal from '../components/loginModal';
 
 const maxCardsToIndexable = 8;
 const DestinationPage = () => {
     const dispatch = useDispatch();
-    const navigate = useNavigate();
 
     const [destinationList, setDestinationList] = useState([]);
     const [destinationDisplay, setDestinationDisplay] = useState([]);
@@ -145,10 +144,6 @@ const DestinationPage = () => {
         searchDestination(province.name, destination.name, searchState?.tags);
     };
 
-    const handleLoginNavigation = async () => {
-        await navigate('/sign-in');
-    };
-
     const setLoginModalVisible = () => {
         if (!loginModal) return;
         loginModal.current.showModal();
@@ -264,48 +259,7 @@ const DestinationPage = () => {
             <JoinUs />
             <Footer />
             <ToastContainer />
-            <dialog
-                ref={loginModal}
-                id="modal-not-login"
-                className="modal font-quicksand"
-            >
-                <div className="modal-box bg-[#252527]">
-                    <form method="dialog">
-                        {/* if there is a button in form, it will close the modal */}
-                        <motion.button
-                            transition={{
-                                ease: 'backOut',
-                                delay: 0,
-                            }}
-                            whileHover={{ scale: 1.125 }}
-                            className="w-fit aspect-square rounded-[360px] hover:bg-gray-700 px-2 absolute right-2 top-2"
-                        >
-                            ✕
-                        </motion.button>
-                    </form>
-                    <h3 className="font-bold text-xl">
-                        You are not{' '}
-                        <span className="text-[#FFA666] font-bold">
-                            Logged In
-                        </span>
-                        , yet
-                    </h3>
-                    <p className="pb-6 font-light tracking-wide">
-                        Login to continue
-                    </p>
-                    <motion.button
-                        transition={{
-                            ease: 'backOut',
-                            delay: 0,
-                        }}
-                        whileHover={{ scale: 1.05 }}
-                        className="rounded-md hover:bg-[#FFA66622] border-solid border-[#FFA666] text-[#FFA666] border-[1px] px-2 py-1 font-semibold tracking-wider"
-                        onClick={handleLoginNavigation}
-                    >
-                        Login right away
-                    </motion.button>
-                </div>
-            </dialog>
+            <LoginModal dialogRef={loginModal} />
         </div>
     );
 };
